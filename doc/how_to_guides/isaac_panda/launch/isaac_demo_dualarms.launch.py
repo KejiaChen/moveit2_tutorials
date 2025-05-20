dfd230daae64b42e9b07c528e3ca3c217f5d7b82
KejiaChen <kejia.chen@tum.de>
2025-05-20 21:44:16 +0200

    add ompl and chomp configuration

--- doc/how_to_guides/isaac_panda/launch/isaac_demo_dualarms.launch.py
+++ doc/how_to_guides/isaac_panda/launch/isaac_demo_dualarms.launch.py
@@ -7,6 +7,28 @@ from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
 from launch_param_builder import ParameterBuilder
+import yaml
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 
 def generate_launch_description():  
@@ -18,7 +40,7 @@ def generate_launch_description():
 
     use_sensone_left = DeclareLaunchArgument(
         "use_sensone_left",
-        default_value="true",
+        default_value="false",
         description="Whether to include the BotaSys external force torque sensor in the left robot model",
     )
 
@@ -37,19 +59,23 @@ def generate_launch_description():
                 "use_sensone_left": LaunchConfiguration("use_sensone_left"),
                 "use_sensone_right": LaunchConfiguration("use_sensone_right"),
             },
-            )
+        )
         .robot_description_semantic(
             file_path="config/panda.srdf.xacro",
             mappings={
                 "use_sensone_left": LaunchConfiguration("use_sensone_left"),
                 "use_sensone_right": LaunchConfiguration("use_sensone_right"),
             },
-            )
+        )
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
-        .planning_pipelines(pipelines=["ompl"])
+        .planning_pipelines(pipelines=["ompl", "chomp"])
         .joint_limits(file_path="config/joint_limits.yaml")
         .to_moveit_configs()
     )
+
+    # Planning Configuration
+    ompl_planning_yaml = load_yaml("dual_arm_panda_moveit_config", "config/ompl_planning.yaml")
+    chomp_planning_yaml = load_yaml("dual_arm_panda_moveit_config", "config/chomp_planning.yaml")
 
     # Load  ExecuteTaskSolutionCapability so we can execute found solutions in simulation
     move_group_capabilities = {
@@ -63,6 +89,8 @@ def generate_launch_description():
         output="screen",
         parameters=[moveit_config.to_dict(),
                     move_group_capabilities,
+                    ompl_planning_yaml,
+                    chomp_planning_yaml,
                     ],
         arguments=["--ros-args", "--log-level", "info"],
     )
@@ -85,6 +113,8 @@ def generate_launch_description():
             moveit_config.robot_description_kinematics,
             moveit_config.planning_pipelines,
             moveit_config.joint_limits,
+            ompl_planning_yaml,
+            chomp_planning_yaml,
         ],
     )
 
